b71d041c0a113b332cd114fc198fecbbc3b9498a
unknown <3122215002103esm@ITLAB.SSN>
2024-11-22 09:30:23 +0530

    fourth commit

--- test_add.py
+++ test_add.py
@@ -8,7 +8,7 @@ def test_add_two_numbers():
     assert add_two_numbers(3.0,40.0) == 43.0
     print('TEST 3: 3.0+40.0 = 43.0 is tested sucessfully')
     assert add_two_numbers(9.0,40) == 49.0
-    print('TEST 4: 3.0+40.0 = 43.0 is tested sucessfully')
+    print('TEST 4: 9.0+40.0 = 49.0 is tested sucessfully')
 
 if __name__ == '__main__':
     test_add_two_numbers()
