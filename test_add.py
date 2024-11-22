479efc4a0611dbd11bbbbd00d3107f9c1fe33f29
unknown <3122215002103esm@ITLAB.SSN>
2024-11-22 09:37:57 +0530

    fifth

--- test_add.py
+++ test_add.py
@@ -1,7 +1,7 @@
 from add import add_two_numbers
 
 def test_add_two_numbers():
-    assert add_two_numbers(3,4) == 7
+    assert add_two_numbers(5,5) == 10
     print('TEST 1: 3+4 = 7 is tested sucessfully')
     assert add_two_numbers('a',3) == 'Can only perform addition on numbers'
     print('TEST 2: "a"+3 gives error is tested sucessfully')
