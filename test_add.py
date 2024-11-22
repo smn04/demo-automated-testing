7cc26eca4f6e6987a0031bee5501ba1a94cc983f
unknown <3122215002103esm@ITLAB.SSN>
2024-11-22 09:29:10 +0530

    third commit

--- test_add.py
+++ test_add.py
@@ -5,6 +5,10 @@ def test_add_two_numbers():
     print('TEST 1: 3+4 = 7 is tested sucessfully')
     assert add_two_numbers('a',3) == 'Can only perform addition on numbers'
     print('TEST 2: "a"+3 gives error is tested sucessfully')
+    assert add_two_numbers(3.0,40.0) == 43.0
+    print('TEST 3: 3.0+40.0 = 43.0 is tested sucessfully')
+    assert add_two_numbers(9.0,40) == 49.0
+    print('TEST 4: 3.0+40.0 = 43.0 is tested sucessfully')
 
 if __name__ == '__main__':
     test_add_two_numbers()
